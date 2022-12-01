56aee9affcab9305001b1a69643789d89293b0ba
Vijay Chitragar <vijaygc112@gmail.com>
2022-12-01 15:32:19 +0530

    Added word and quilibrium solutions

---
+++ src/main/java/com/basic/NoOfWords.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+/*Multiple approaches available
+1. Loop through each character -> if separator is hit > Increment the counter
+2. Use String.split() method -> use string array.length
+*/
+public class NoOfWords {
+
+    public static void main(String[] args) {
+        String s = "This is Word  Count ";
+        System.out.println("Given String is " + s);
+        System.out.println("Word Count is " + find_count_via_split_method(s));
+    }
+
+    private static int find_count_via_split_method(String s) {
+
+        String[] strings = s.split("\\s+");
+        return strings.length;
+
+
+    }
+
+
+}
---
+++ src/main/java/com/interview/EquilibriumPoint.java
@@ -0,0 +1,41 @@
+package com.interview;
+
+// Brute force approach will have o(n^2) complexity
+// below solution is with O(n) complexity
+
+public class EquilibriumPoint {
+
+
+    public static void main(String[] args) {
+        int[] array = {2,0,4,5,2};
+        int length = array.length;
+        System.out.println("Equilibrium index is : " + equilibriumIndex(array, length));
+
+    }
+
+    private static int equilibriumIndex(int[] array, int length) {
+        int i;
+        int leftsum = 0, sum = 0;
+
+        for (i = 0; i < length; i++)
+            sum += array[i];
+
+        for (i = 0; i < length; i++) {
+            sum -= array[i];
+
+            if (leftsum == sum)
+                return i;
+
+            leftsum += array[i];
+        }
+
+        return -1;
+
+
+    }
+
+}
+
+
+
+
