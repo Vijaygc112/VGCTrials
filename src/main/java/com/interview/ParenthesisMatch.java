b80eca38ca280d5549b495ace25feb023bca2f1d
Vijay Chitragar <vijaygc112@gmail.com>
2023-07-08 18:10:35 +0530

    Modified Paranthesis match solution

--- src/main/java/com/interview/ParenthesisMatch.java
+++ src/main/java/com/interview/ParenthesisMatch.java
@@ -12,8 +12,9 @@ import java.util.Stack;
 public class ParenthesisMatch {
 
     public static void main(String[] args) {
-        String string = "[](){}{";
-        Boolean bool = checkParenthesis(string);
+        //  String string = "package com.basic; public class OlaSolution { public static void main(String[] args) { int a[] = {1,2,3,4}; int b[] = {2, 5, 6}; //VIJAY JAYVI // VIJAY JAYIV //1 2 3 4 // 2 5 6 // 1 3 4 5 6 printUniqueelements(a, b); } private static void printUniqueelements(int[] a, int[] b) { int[] c = new int[10]; int[] d = new int[10]; for (int i = 0; i < a.length; i++) { for (int j = 0; j < i ; j++) { if(a[i] != a[j]){ c[i] = a[i]; } } } for (int i = 0; i < c.length; i++) { System.out.println(c[i]); } } } ";
+            String string = "{aasas([])}as}";
+            Boolean bool = checkParenthesis(string);
 
         if(bool)
             System.out.println("Compiled Successfully" );
@@ -28,22 +29,20 @@ public class ParenthesisMatch {
             char c = string.charAt(i);
             if (c == '{' || c == '(' || c == '[' ){
                 stack.push(c);
+                continue;
             }
 
             else if(!stack.isEmpty() &&
-                    ( c == '}' && stack.peek() == '{') ||
+                    (( c == '}' && stack.peek() == '{') ||
                     ( c == ')' && stack.peek() == '(') ||
-                    ( c == ']' && stack.peek() == '[') ){
-
+                    ( c == ']' && stack.peek() == '[') )){
                         stack.pop();
             }
-            else
-                stack.push(c);
+             else if (c == '}' || c== ')' || c== ']')   // this is needed to handle if there are any additional closing brackets at the end
+                   stack.push(c);
         }
-        if (stack.isEmpty())
-            return true;
-        else
-            return false;
+        return (stack.isEmpty());
+
     }
 
 }
